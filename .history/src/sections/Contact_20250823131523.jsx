3c696f212b7184581d7b81c54153122a83e9216f
swarnabha <swarnabhadutta909@gmail.com>
2025-08-23 13:16:34 +0530

    update import syntax

--- .history/src/sections/Contact_20250823131523.jsx
+++ .history/src/sections/Contact_20250823131523.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import TitleHeader from "../components/TitleHeader";
-
+// import ContactExperience from "../components/models/contact/ContactExperience.jsx";
 import ContactExperience from "../components/models/contact/ContactExperience.jsx"
 
 const Contact = () => {
